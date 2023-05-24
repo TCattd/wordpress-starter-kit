7b959b7a59f322bd67ec129e08ec30e58434a7c9
Esteban <esteban@attitude.cl>
2023-05-24 17:02:16 -0400

    picostrap child theme 2.1.0

--- themes/picostrap5-child-base/functions.php
+++ themes/picostrap5-child-base/functions.php
@@ -19,6 +19,56 @@
 
 */
 
+// DE-ENQUEUE PARENT THEME BOOTSTRAP JS BUNDLE
+add_action( 'wp_print_scripts', function(){
+    wp_dequeue_script( 'bootstrap5' );
+}, 100 );
+
+// ENQUEUE THE BOOTSTRAP JS BUNDLE (AND EVENTUALLY MORE LIBS) FROM THE CHILD THEME DIRECTORY
+add_action( 'wp_enqueue_scripts', function() {
+    //enqueue js in footer, defer
+    wp_enqueue_script( 'bootstrap5-childtheme', get_stylesheet_directory_uri() . "/js/bootstrap.bundle.min.js#deferload", array(), null, true );
+    
+    //optional: lottie (maybe...)
+    //wp_enqueue_script( 'lottie-player', 'https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js#deferload', array(), null, true );
+
+    //optional: rellax 
+    //wp_enqueue_script( 'rellax', 'https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js#deferload', array(), null, true );
+
+}, 101);
+
+    
+    
+// ENQUEUE YOUR CUSTOM JS FILES, IF NEEDED 
+add_action( 'wp_enqueue_scripts', function() {	   
+    
+    //UNCOMMENT next row to include the js/custom.js file globally
+    //wp_enqueue_script('custom', get_stylesheet_directory_uri() . '/js/custom.js#deferload', array(/* 'jquery' */), null, true); 
+
+    //UNCOMMENT next 3 rows to load the js file only on one page
+    //if (is_page('mypageslug')) {
+    //    wp_enqueue_script('custom', get_stylesheet_directory_uri() . '/js/custom.js#deferload', array(/* 'jquery' */), null, true); 
+    //}  
+
+}, 102);
+
+// OPTIONAL: ADD MORE NAV MENUS
+//register_nav_menus( array( 'third' => __( 'Third Menu', 'picostrap' ), 'fourth' => __( 'Fourth Menu', 'picostrap' ), 'fifth' => __( 'Fifth Menu', 'picostrap' ), ) );
+// THEN USE SHORTCODE:  [lc_nav_menu theme_location="third" container_class="" container_id="" menu_class="navbar-nav"]
+
+
+// CHECK PARENT THEME VERSION as Bootstrap 5.2 requires an updated SCSSphp, so picostrap5 v2 is required
+add_action( 'admin_notices', function  () {
+    if( (pico_get_parent_theme_version())>=2.1) return; 
+	$message = __( 'This Child Theme requires at least Picostrap Version 2.1.0  in order to work properly. Please update the parent theme.', 'picostrap' );
+	printf( '<div class="%1$s"><h1>%2$s</h1></div>', esc_attr( 'notice notice-error' ), esc_html( $message ) );
+} );
+
+// FOR SECURITY: DISABLE APPLICATION PASSWORDS. Remove if needed (unlikely!)
+add_filter( 'wp_is_application_passwords_available', '__return_false' );
+
+// ADD YOUR CUSTOM PHP CODE DOWN BELOW /////////////////////////
+
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
@@ -34,45 +84,3 @@ if ( is_array( $picostrap_includes ) && ! empty( $picostrap_includes ) ) {
         require_once get_stylesheet_directory() . '/includes' . $file;
     }
 }
-
-// DE-ENQUEUE PARENT THEME BOOTSTRAP JS BUNDLE
-add_action( 'wp_print_scripts', function(){
-    wp_dequeue_script( 'bootstrap5' );
-}, 100 );
-
-// ENQUEUE THE BOOTSTRAP JS BUNDLE FROM THE CHILD THEME DIRECTORY
-add_action( 'wp_enqueue_scripts', function() {
-    //enqueue js in footer, async
-    wp_enqueue_script( 'bootstrap5-childtheme', get_stylesheet_directory_uri() . "/js/bootstrap.bundle.min.js#asyncload", array(), null, true );
-} ,101);
-
-// ENQUEUE YOUR CUSTOM JS FILES, IF NEEDED 
-add_action( 'wp_enqueue_scripts', function() {	   
-    
-    //UNCOMMENT next row to include the js/custom.js file globally
-    //wp_enqueue_script('custom', get_stylesheet_directory_uri() . '/js/custom.js', array(/* 'jquery' */), null, true); 
-
-    //UNCOMMENT next 3 rows to load the js file only on one page
-    //if (is_page('mypageslug')) {
-    //    wp_enqueue_script('custom', get_stylesheet_directory_uri() . '/js/custom.js', array(/* 'jquery' */), null, true); 
-    // }  
-
-});
-
-// OPTIONAL: ADD MORE NAV MENUS
-//register_nav_menus( array( 'third' => __( 'Third Menu', 'picostrap' ), 'fourth' => __( 'Fourth Menu', 'picostrap' ), 'fifth' => __( 'Fifth Menu', 'picostrap' ), ) );
-// THEN USE SHORTCODE:  [lc_nav_menu theme_location="third" container_class="" container_id="" menu_class="navbar-nav"]
-
-
-// CHECK PARENT THEME VERSION as Bootstrap 5.2 requires an updated SCSSphp, so picostrap5 v2 is required
-add_action( 'admin_notices', function  () {
-    if( (pico_get_parent_theme_version())>=2) return;
-	$class = 'notice notice-error';
-	$message = __( 'This Child Theme requires at least Picostrap Version 2 for the SCSS compiler to work properly. Please update the parent theme.', 'picostrap' );
-	printf( '<div class="%1$s"><h1>%2$s</h1></div>', esc_attr( $class ), esc_html( $message ) );
-} );
-
-// FOR SECURITY: DISABLE APPLICATION PASSWORDS. Remove if needed (unlikely!)
-add_filter( 'wp_is_application_passwords_available', '__return_false' );
-
-// ADD YOUR CUSTOM PHP CODE DOWN BELOW /////////////////////////
